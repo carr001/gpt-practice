b97d5b5d4a5e8e59c59fc0c3fb25f6c1011ad4f3
carr001 <carr_001@163.com>
2024-03-22 00:05:46 +0800

    update

--- .ipynb_checkpoints/full_code-checkpoint.py
+++ .ipynb_checkpoints/full_code-checkpoint.py
@@ -92,12 +92,12 @@ class MultiHeadAttention(nn.Module):
 
     def __init__(self, num_heads, head_size):
         super().__init__()
+        # multi-head有点类似group convolution
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        # multi-head有点类似group convolution，输出的conv的个数，heads的数量可以自定义
         out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.dropout(self.proj(out))
         return out
